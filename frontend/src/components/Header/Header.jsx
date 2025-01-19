e04c03d9908c903c0c0d986e122707aead1c0f47
nikita <funnymoments610@gmail.com>
2025-01-19 23:32:36 +0100

    feat(header): стилизовать и добавить функционал в header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,15 +1,83 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import "./Header.scss"
+import logoVibixImage from '../../assets/icons/logo.svg';
+import searchIcon from '../../assets/icons/search.svg'
+import themeIcon from '../../assets/icons/theme.svg'
+import { setLanguage, setTheme, setIsLogged } from '../../store/reducers/clientReducer'
+import { useDispatch } from 'react-redux';
+
 
 export default function Header() {
-    const currentPage = useSelector(state => state.client.currentPage)
+    const dispatch = useDispatch()
+
+    const language = useSelector(state => state.client.language)
+    const theme = useSelector(state => state.client.theme)
+    const isLogged = useSelector(state => state.client.isLogged)
+
+    const translations = {
+        ua: {
+            navMain: 'Головна',
+            navFilms: 'Фільми',
+            navFavorites: 'Збережене',
+            accountButton: 'Увійти',
+            languageSign: 'UA'
+        },
+        en: {
+            navMain: 'Main',
+            navFilms: 'Films',
+            navFavorites: 'Favorites',
+            accountButton: 'LogIn',
+            languageSign: 'EN'
+        }
+    }
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme)
+    }, [theme])
 
     return (
-        <>
-            <h1>Header</h1>
-            {currentPage !== 'search' && <p>lorem ipsum</p>}
-            {currentPage !== 'search' && <p>lorem gfklngkjdafngjnd</p>}
-            <h4>gndfjgjdifhjigjd</h4>
-        </>
+        <header className='header'>
+            <div className="header__inner container">
+                <div className="header__row row">
+                    <div className="header__column header__column--1 column">
+                        <a href="#" className="header__link--image">
+                            {/* // ! todo обрезать края изображения */}
+                            <img src={logoVibixImage} alt="Vibix" className="header__logo" width={110} height={80} />
+                        </a>
+                    </div>
+                    <div className="header__column header__column--2 column">
+                        <nav className="header__navigation">
+                            <ul className="header__list--navigation">
+                                <li className="header__list-item">
+                                    <a href="#" className="header__link">{translations[language].navMain}</a>
+                                </li>
+                                <li className="header__list-item">
+                                    <a href="#" className="header__link">{translations[language].navFilms}</a>
+                                </li>
+                                <li className="header__list-item">
+                                    <a href="#" className="header__link">{translations[language].navFavorites}</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="header__column header__column--3 column">
+                        <div className="header__tools">
+                            <ul className='header__list--tools'>
+                                <a href="#" className='header__link--image'>
+                                    <img src={searchIcon} alt="search" />
+                                </a>
+                                <button className="header__button--theme" onClick={() => dispatch(setTheme())}>T</button>
+                                <button className="header__button--language" onClick={() => dispatch(setLanguage())}>
+                                    {translations[language].languageSign}
+                                </button>
+                            </ul>
+                        </div>
+                        {isLogged === true ? <img className='header__image--account' src={themeIcon} alt='Account' /> : <button class='header__button--enter'>{translations[language].accountButton}</button>}
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
--- frontend/src/store/reducers/clientReducer.js
+++ frontend/src/store/reducers/clientReducer.js
@@ -2,13 +2,27 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const clientSlice = createSlice({
     name: 'client',
-    initialState: { currentPage: 'main' },
+    initialState: {
+        language: 'ua',
+        currentPage: 'main',
+        theme: 'dark',
+        isLogged: false,
+    },
     reducers: {
+        setLanguage: (state) => {
+            state.language = state.language === 'ua' ? 'en' : 'ua'
+        },
         setCurrentPage: (state, action) => {
             state.currentPage = action.payload
+        },
+        setTheme: (state) => {
+            state.theme = state.theme === 'light' ? 'dark' : 'light'
+        },
+        setIsLogged: (state) => {
+            state.isLogged = !state.isLogged
         },
     },
 })
 
-export const { setCurrentPage } = clientSlice.actions
+export const { setLanguage, setCurrentPage, setTheme, setIsLogged } = clientSlice.actions
 export default clientSlice.reducer
