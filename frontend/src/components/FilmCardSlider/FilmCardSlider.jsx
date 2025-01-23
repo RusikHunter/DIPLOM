a6bc669943511995536f49c3a1a0faab73945f0e
nikita <funnymoments610@gmail.com>
2025-01-23 23:45:01 +0100

    feat(filmcardslider): внести правки для filmcardslider и filmcard

--- frontend/src/components/FilmCardSlider/FilmCardSlider.jsx
+++ frontend/src/components/FilmCardSlider/FilmCardSlider.jsx
@@ -17,17 +17,6 @@ import 'swiper/css/pagination';
 export default function FilmCardSlider({ title }) {
     const swiperRef = useRef(null)
 
-    // отключать скролл после 3го слайда
-    const [currentSlide, setCurrentSlide] = useState(0)
-    const maxSlides = 18
-    const handleSlideChange = () => {
-        const swiper = swiperRef.current.swiper
-        const newSlide = swiper.realIndex
-        setCurrentSlide(newSlide)
-    }
-    const canSlideNext = currentSlide < maxSlides
-    //
-
     const handlePrev = () => {
         swiperRef.current.swiper.slidePrev()
     }
@@ -45,12 +34,12 @@ export default function FilmCardSlider({ title }) {
                     <a className="film-card-slider__link" href="#">Дивитись усе</a>
 
                     <div className="film-card-slider__navigation-wrap">
-                        <button className="film-card-slider__navigation-button--left" onClick={handlePrev} disabled={swiperRef.current?.swiper?.isBeginning}>
+                        <button className="film-card-slider__navigation-button--left" onClick={handlePrev}>
                             <svg width="10" height="20" viewBox="0 0 10 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M8.12807 17.5542L1.4989 9.59917L8.12807 1.64417" stroke="#767676" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
                             </svg>
                         </button>
-                        <button className="film-card-slider__navigation-button--right" onClick={handleNext} disabled={!canSlideNext}>
+                        <button className="film-card-slider__navigation-button--right" onClick={handleNext}>
                             <svg width="10" height="20" viewBox="0 0 10 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M1.7489 1.646L8.37808 9.601L1.7489 17.556" stroke="#767676" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
                             </svg>
@@ -59,41 +48,39 @@ export default function FilmCardSlider({ title }) {
                 </div>
                 <div className="film-card-slider__row film-card-slider__row--2 row">
                     <Swiper
+                        className="film-card-slider__swiper"
                         ref={swiperRef}
                         spaceBetween={15} // Расстояние между слайдами
                         slidesPerView="auto" // Включаем авторазмер слайдов
                         slidesPerGroup={6} // Прокручиваем по одному слайду
                         loop={false} // Отключаем бесконечный цикл
-                        style={{ paddingRight: '200px' }} // Отступ справа на последнем слайде
                         speed={700}
-                        onSlideChange={handleSlideChange}
                         simulateTouch={false}
                         allowTouchMove={false}
                     >
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
-                        <SwiperSlide style={{ width: '245px' }}><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
+                        <SwiperSlide className="film-card-slider__slide"><FilmCard /></SwiperSlide>
                     </Swiper>
                 </div>
             </div>
