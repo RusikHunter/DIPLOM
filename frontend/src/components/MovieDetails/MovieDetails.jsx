10083dfb5fec2be92c2b14adcdb5ea3338b2f2a4
nikita <funnymoments610@gmail.com>
2025-01-27 22:57:30 +0100

    feat(moviedetails): стилизовать компонент moviedetails

--- frontend/src/components/MovieDetails/MovieDetails.jsx
+++ frontend/src/components/MovieDetails/MovieDetails.jsx
@@ -13,17 +13,27 @@ export default function MovieDetails() {
                             <h5 className="movie-details__title">Інформація</h5>
 
                             <dl className="movie-details__info-description-list">
-                                <dt className="movie-details__info-description-term">Режисер:</dt>
-                                <dd className="movie-details__info-description-definition">Роберт Еггерс</dd>
+                                <div className="movie-details__info-description-list-item-wrap">
+                                    <dt className="movie-details__info-description-term">Режисер:</dt>
+                                    <dd className="movie-details__info-description-definition">Роберт Еггерс</dd>
+                                </div>
 
-                                <dt className="movie-details__info-description-term">Сценаристи:</dt>
-                                <dd className="movie-details__info-description-definition">Роберт Еггерс Генрік Галеєн Брем Стокер</dd>
+                                <div className="movie-details__info-description-list-item-wrap">
+                                    <dt className="movie-details__info-description-term">Сценаристи:</dt>
+                                    <dd className="movie-details__info-description-definition">Роберт Еггерс Генрік Галеєн Брем Стокер</dd>
+                                </div>
 
-                                <dt className="movie-details__info-description-term">Актори:</dt>
-                                <dd className="movie-details__info-description-definition">Лілі-Роуз Депп Ніколас Голт Білл Скашгорд Аарон Тейлор-Джонсон Віллем Дефо</dd>
 
-                                <dt className="movie-details__info-description-term">Країни:</dt>
-                                <dd className="movie-details__info-description-definition">Чехія США</dd>
+                                <div className="movie-details__info-description-list-item-wrap">
+                                    <dt className="movie-details__info-description-term">Актори:</dt>
+                                    <dd className="movie-details__info-description-definition">Лілі-Роуз Депп Ніколас Голт Білл Скашгорд Аарон Тейлор-Джонсон Віллем Дефо</dd>
+                                </div>
+
+
+                                <div className="movie-details__info-description-list-item-wrap">
+                                    <dt className="movie-details__info-description-term">Країни:</dt>
+                                    <dd className="movie-details__info-description-definition">Чехія США</dd>
+                                </div>
                             </dl>
                         </div>
                     </div>
@@ -102,7 +112,7 @@ export default function MovieDetails() {
 
                         <div className="movie-details__reviews-wrap">
                             <form className="movie-details__review-form">
-                                <img src={avatar} className="movie-details__review-image" alt="avatar" />
+                                <img src={avatar} className="movie-details__review-image--my-account" alt="avatar" />
 
                                 <input className="movie-details__review-input--text" type="text" placeholder="Відгук" />
 
@@ -115,55 +125,75 @@ export default function MovieDetails() {
                                 <li className="movie-details__review">
                                     <img src={avatar} className="movie-details__review-image" alt="avatar" />
 
-                                    <p className="movie-details__review-text">
-                                        Bullshit. Worst I'd ever seen, i hope, fuckin author will die by rock in his mf face
-                                    </p>
+                                    <div className="movie-details__review-author-info-wrap">
+                                        <span className="movie-details__review-author-username">
+                                            Author123
+                                        </span>
+
+                                        <p className="movie-details__review-text">
+                                            Bullshit. Worst I'd ever seen, i hope, fuckin author will die by rock in his mf face
+                                        </p>
+                                    </div>
                                 </li>
                                 <li className="movie-details__review">
                                     <img src={avatar} className="movie-details__review-image" alt="avatar" />
 
-                                    <p className="movie-details__review-text">
-                                        Bullshit. Worst I'd ever seen, i hope, fuckin author will die by rock in his mf face
-                                    </p>
+                                    <div className="movie-details__review-author-info-wrap">
+                                        <span className="movie-details__review-author-username">
+                                            Author123
+                                        </span>
+
+                                        <p className="movie-details__review-text">
+                                            Bullshit. Worst I'd ever seen, i hope, fuckin author will die by rock in his mf face
+                                        </p>
+                                    </div>
                                 </li>
                                 <li className="movie-details__review">
-                                    <img src={avatar} className="movie-details__review-image" alt="avatar" />
+                                    <img src={avatar} className="movie-details__review-image" alt="avatar" width={48} height={48} />
 
-                                    <p className="movie-details__review-text">
-                                        Bullshit. Worst I'd ever seen, i hope, fuckin author will die by rock in his mf face
-                                    </p>
+                                    <div className="movie-details__review-author-info-wrap">
+                                        <span className="movie-details__review-author-username">
+                                            Author123
+                                        </span>
+
+                                        <p className="movie-details__review-text">
+                                            Bullshit. Worst I'd ever seen, i hope, fuckin author will die by rock in his mf face
+                                        </p>
+                                    </div>
                                 </li>
                             </ul>
-
-                            <button
-                                className="movie-details__button--add-more"
-                            // style={{
-                            //     display: visibleSlideSets >= slideSets.length ? "none" : "flex",
-                            // }}
-                            // onClick={handleAddMore}
-                            >
-                                <svg
-                                    width="38"
-                                    height="36"
-                                    viewBox="0 0 38 36"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        d="M32.8443 10.0278C30.0644 5.22876 24.8685 2 18.9176 2C12.1703 2 6.39372 6.1507 4.00867 12.0347M27.9612 12.0347H36V4.00695M5.15575 26.0833C7.93564 30.8824 13.1315 34.1111 19.0824 34.1111C25.8297 34.1111 31.6063 29.9604 33.9913 24.0764M10.0388 24.0764H2V32.1042"
-                                        stroke="white"
-                                        strokeWidth="3"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                    />
-                                </svg>
-                                Показати ще
-                            </button>
                         </div>
                     </div>
                 </div>
-                <div className="movie-details__row movie-details__row--4 row"></div>
+
+                <div className="movie-details__row movie-details__row--4 row">
+                    <button
+                        className="movie-details__button--add-more"
+                    // style={{
+                    //     display: visibleSlideSets >= slideSets.length ? "none" : "flex",
+                    // }}
+                    // onClick={handleAddMore}
+                    >
+                        <svg
+                            width="38"
+                            height="36"
+                            viewBox="0 0 38 36"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M32.8443 10.0278C30.0644 5.22876 24.8685 2 18.9176 2C12.1703 2 6.39372 6.1507 4.00867 12.0347M27.9612 12.0347H36V4.00695M5.15575 26.0833C7.93564 30.8824 13.1315 34.1111 19.0824 34.1111C25.8297 34.1111 31.6063 29.9604 33.9913 24.0764M10.0388 24.0764H2V32.1042"
+                                stroke="white"
+                                strokeWidth="3"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            />
+                        </svg>
+                        Показати ще
+                    </button>
+                </div>
             </div>
         </section>
     )
 }
+
