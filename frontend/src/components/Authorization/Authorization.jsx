33ff7f944935c0fd49bb8941c277c126e252ebcf
dmfshove <funnymoments610@gmail.com>
2025-02-05 22:57:01 +0100

    feat(authorization): создать разметку для компонента authorization

--- frontend/src/components/Authorization/Authorization.jsx
+++ frontend/src/components/Authorization/Authorization.jsx
@@ -3,6 +3,33 @@ import './Authorization.scss'
 
 export default function Authorization() {
     return (
-        <></>
+        <section className="authorization">
+            <div className="authorization__inner container">
+                <div className="authorization__row row">
+                    <div className="authorization__column column">
+                        <h1 className="authorization__title">Авторизация</h1>
+
+                        <form className="authorization__form">
+                            <label htmlFor="authorizationEmail" className="authorization__label authorization__label--email">
+                                <input className="authorization__input authorization__input--email" type="email" name="authorizationEmail" required autocomplete="email" />
+                            </label>
+
+                            <label htmlFor="authorizationPassword" className="authorization__label authorization__label--password">
+                                <input className="authorization__input authorization__input--password" type="password" name="authorizationPassword" required minlength="6" />
+                            </label>
+
+                            <button type="submit" className="authorization__button authorization__button--submit">
+                                Вход
+                            </button>
+                        </form>
+
+                        <span className="authorization__text--to-reg">
+                            Впервые на Vibix?
+                            <a href="#" className="authorization__link--to-reg">Зарегистрируйтесь</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
--- frontend/src/pages/AuthPage/AuthPage.jsx
+++ frontend/src/pages/AuthPage/AuthPage.jsx
@@ -15,7 +15,7 @@ export default function AuthPage() {
 
     return (
         <>
-            <Registration />
+            {/* <Registration /> */}
             <Authorization />
         </>
     )
