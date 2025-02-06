8cbb5ab8285c5ef1252f9563c8481b9cd4caa366
dmfshove <funnymoments610@gmail.com>
2025-02-06 14:54:13 +0100

    feat(accountpage): создать разметку для accountpage

--- frontend/src/pages/AccoutPage/AccountPage.jsx
+++ frontend/src/pages/AccoutPage/AccountPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { setCurrentPage } from '../../store/reducers/clientReducer'
+import userAvatar from '../../assets/icons/avatar.png'
 
 export default function AccountPage() {
     const dispatch = useDispatch()
@@ -12,7 +13,41 @@ export default function AccountPage() {
 
     return (
         <>
-            <p>AccountPage</p>
+            <section className="section section__account accout">
+                <div className="accout__row accout__row--1 row">
+                    <h1 className="account__title">Аккаунт пользователя</h1>
+                </div>
+                <div className="accout__row accout__row--2 row">
+                    <div className="account__column account__column--1 column">
+                        <img src={userAvatar} alt="Avatar" className="account__avatar" />
+                    </div>
+                    <div className="account__column account__column--2 column">
+                        <div className="account__details-wrap">
+                            <h3 className="account__subtitle">Данные про пользователя</h3>
+
+                            <label htmlFor="accountInputUsername" className='account__label account__label--username'>
+                                <input type="text" name="accountInputUsername" className="account__input account__input--username" placeholder='dmfshove' />
+                                <button type="button" className="account__button--change">Изменить</button>
+                            </label>
+
+                            <label htmlFor="accountInputEmail" className='account__label account__label--email'>
+                                <input type="email" name="accountInputEmail" className="account__input account__input--email" placeholder="funnymoments610@gmail.com" />
+                            </label>
+
+                        </div>
+
+                        <div className="account__plan-wrap">
+                            <h3 className="account__subtitle">План</h3>
+                            <span className="account__link--plan">Выбрать другой план</span>
+
+                            <div className="account__plan-block">
+                                <h6 className="account__plan-title">Премиум</h6>
+                                <span className="account__plan-details">4K</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
