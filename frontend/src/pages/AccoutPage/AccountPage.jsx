38c00122d2f18f06917c3ec7d08e9d2a19997f7a
dmfshove <funnymoments610@gmail.com>
2025-02-08 17:28:36 +0100

    feat(account): добавить возможность менять имя пользователя

--- frontend/src/pages/AccoutPage/AccountPage.jsx
+++ frontend/src/pages/AccoutPage/AccountPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setCurrentPage } from '../../store/reducers/clientReducer'
 import userAvatar from '../../assets/icons/useravatar.png'
@@ -7,6 +7,7 @@ import translationsJSON from '../../assets/translations.json'
 import './AccountPage.scss'
 import { useNavigate } from 'react-router-dom';
 import { setIsLogged, setCurrentUser } from '../../store/reducers/clientReducer'
+import axios from 'axios'
 
 export default function AccountPage() {
     const dispatch = useDispatch()
@@ -16,6 +17,9 @@ export default function AccountPage() {
     const isLogged = useSelector(state => state.client.isLogged)
     const translations = translationsJSON
 
+    const textUsernameUnsuccessRef = useRef()
+    const textUsernameSuccessRef = useRef()
+
     const [usernameInputValue, setUsernameInputValue] = useState(currentUser.username || "")
     const [emailInputValue, setEmailInputValue] = useState(currentUser.email || "")
     const [planValue, setPlanValue] = useState(currentUser.plan || "")
@@ -24,11 +28,39 @@ export default function AccountPage() {
         setUsernameInputValue(event.target.value);
     }
 
+    const handleChangeUsername = async () => {
+        try {
+            const response = await axios.put(`http://localhost:8000/users/${currentUser.email}`, {
+                username: usernameInputValue,
+            })
+
+            dispatch(setCurrentUser({
+                ...currentUser,
+                username: usernameInputValue
+            }))
+
+            textUsernameSuccessRef.current.style.display = "block"
+
+            setTimeout(() => {
+                textUsernameSuccessRef.current.style.display = "none"
+            }, 3000)
+        } catch (error) {
+            textUsernameUnsuccessRef.current.style.display = "block"
+
+            setTimeout(() => {
+                textUsernameUnsuccessRef.current.style.display = "none"
+            }, 3000)
+        }
+    }
+
     useEffect(() => {
         dispatch(setCurrentPage('account'))
     }, [])
 
     useEffect(() => {
+        if (currentUser && Object.keys(currentUser).length > 0) {
+            localStorage.setItem('user', JSON.stringify(currentUser));
+        }
         if (currentUser.username) {
             setUsernameInputValue(currentUser.username);
         }
@@ -57,10 +89,10 @@ export default function AccountPage() {
 
                                 <label htmlFor="accountInputUsername" className='account__label account__label--username'>
                                     <input type="text" name="accountInputUsername" className="account__input account__input--username" placeholder={translations[language].account.username} value={usernameInputValue} onChange={handleChange} />
-                                    <button type="button" className="account__button--change" onClick={() => {
-                                        console.log('a')
-                                    }}>{translations[language].account.change}</button>
+                                    <button type="button" className="account__button--change" onClick={handleChangeUsername}>{translations[language].account.change}</button>
                                 </label>
+                                <p ref={textUsernameUnsuccessRef} className="account__success-text--unsuccess">{translations[language].accountForm.usernameAlreadyTaken}</p>
+                                <p ref={textUsernameSuccessRef} className="account__success-text--success">{translations[language].accountForm.usernameChanged}</p>
 
                                 <label htmlFor="accountInputEmail" className='account__label account__label--email'>
                                     <input type="email" name="accountInputEmail" className="account__input account__input--email" value={emailInputValue} disabled />
